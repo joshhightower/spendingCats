e3963ed6f8d7c4d195fd5b46e7f30fdf81bf2b10
Stephen Hightower <joshhightower@aol.com>
2018-02-04 16:29:51 -0800

    generally working. still trying to get the dropdown collapse to work on the data-target collapse

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,35 @@ import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent} from './home/home.component';
+import { TrackingComponent } from './tracking/tracking.component';
+import { BudgetsComponent } from './budgets/budgets.component';
+import { TrendsComponent } from './trends/trends.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'tracking', component: TrackingComponent },
+  { path: 'budgets', component: BudgetsComponent },
+  { path: 'trends', component: TrendsComponent },
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TrackingComponent,
+    BudgetsComponent,
+    TrendsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
